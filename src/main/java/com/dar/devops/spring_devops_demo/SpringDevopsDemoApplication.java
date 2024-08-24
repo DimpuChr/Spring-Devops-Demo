51d0baddfa2eacabf126c99bdd89e8c7cbfcd545
DimpuChr <167059422+DimpuChr@users.noreply.github.com>
2024-08-24 13:27:43 +0530

    fresh

---
+++ src/main/java/com/dar/devops/spring_devops_demo/SpringDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dar.devops.spring_devops_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevopsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dar/devops/spring_devops_demo/SpringDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dar.devops.spring_devops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
